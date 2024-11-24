a9412cbd5ff0702e16c186238802304e59ca2899
zinovy124 <developerzinovy@gmail.com>
2024-11-24 15:39:45 +0900

    유져 추가 기능 구현 실패

--- OrderService/src/main/java/com/example/orderservice/dto/UserDto.java
+++ OrderService/src/main/java/com/example/orderservice/dto/UserDto.java
@@ -1,4 +1,18 @@
 package com.example.orderservice.dto;
 
+import com.example.orderservice.entity.User;
+
 public record UserDto(int id, String name) {
+    public static UserDto fromEntity(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getName()
+        );
+    }
+
+    public static User toEntity(UserDto userDto) {
+        return new User(
+                userDto.name
+        );
+    }
 }
